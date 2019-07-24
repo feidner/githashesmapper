6821280e2dab0df8ed3e7b5132f723085177e877
Henrik <henrik.feidner@siemens.com>
2019-07-24 09:05:23 +0200

    logger framework und test plugin hinzugefuegt

--- src/main/java/mapper/GitLabHashMappingApplication.java
+++ src/main/java/mapper/GitLabHashMappingApplication.java
@@ -1,13 +1,14 @@
 package mapper;
 
+import org.apache.logging.log4j.LogManager;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackageClasses = { SecurityConfig.class })
 public class GitLabHashMappingApplication {
 
     public static void main(String[] args) {
+        LogManager.getLogger(GitLabHashMappingApplication.class.getSimpleName()).info("START APPLICATION");
         SpringApplication.run(GitLabHashMappingApplication.class);
     }
 }
